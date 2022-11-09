dfd097a21b594172facc04a2530f1311e643fca5
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-11-09 22:32:02 +0300

    add category transaction and other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :categories do
+    resources :purchases
+  end
+
   devise_for :users
   root 'categories#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
